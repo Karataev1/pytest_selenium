e5d5d7232a81da9e2887898813a4d5489d0f9edd
Karataev <ivan.karataev19@gmail.com>
2025-06-13 06:56:03 +0300

    commit

--- qa-practice/test_input_text.py
+++ qa-practice/test_input_text.py
@@ -11,22 +11,32 @@ import allure
 
 @allure.feature('text_input_1')
 class TestInputPage:
-
-
+    """
+    Здесь тестируется первая вкладка страницы Input Text
+    """
     @allure.title('test is_displayed')
     def test_line_is_displayed(self,page):
+        """
+        Тест проверяет отображение поля ввода для пользователя
+        """
         with allure.step('Проверяю отображение поля для ввода'):
             assert page.input_line_is_displayed()
 
 
     @allure.title('test is_enabled')
     def test_line_is_enabled(self,page):
+        """
+        Тест проверяет доступность поля ввода для пользователя
+        """
         with allure.step('Проверяю доступность для пользователя поля ввода'):
             assert page.input_line_is_enabled()
 
 
     @allure.title('test send_text')
     def test_send_text(self,page, text='ok123_-AA'):
+        """
+        Тест вводит текст за пользователя, который соответсвует требованиям
+        """
         with allure.step(f'Отчищаю поле ввода и ввожу свои данные [{text}]'):
             page.clear_text_line().text_line_set_text(text)
             page.press_enter_in_line()
@@ -42,6 +52,11 @@ class TestInputPage:
     @pytest.mark.negative
     @pytest.mark.parametrize('invalid_number_of_character', ['1', GenerateText.generate_char(26)])
     def test_invalid_number_of_characters(self,page, invalid_number_of_character):
+        """
+        Тест вводит неверные данные за пользователя, а именно
+        запрещеные для этого поля символы (@=+ и др.). Для того чтобы текст прошел условие (не менее 2 символов)
+        в строку подставляется слово 'test'
+        """
         with allure.step(f'Отчищаю поле ввода и ввожу свои данные [{invalid_number_of_character}]'):
             page.clear_text_line()
             page.text_line_set_text(invalid_number_of_character)
@@ -59,6 +74,9 @@ class TestInputPage:
     @pytest.mark.negative
     @pytest.mark.parametrize("invalid_string", GenerateText.get_invalid_characters())
     def test_invalid_characters_in_input(self,page, invalid_string):
+        """
+        Тест вводит данные размером в один символ и больше 25, задача - получить ошибку от сайта
+        """
         with allure.step(f'Отчищаю поле ввода и ввожу свои данные [{invalid_string}]'):
             page.clear_text_line()
             page.text_line_set_text(invalid_string)
